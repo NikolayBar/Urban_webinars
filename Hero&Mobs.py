a52e0f07258aefc2e26801a0a74ed74f2a43239e
NikolayBar <nikolbar@yandex.ru>
2024-08-31 02:02:21 +0300

    edit Hero&Mobs.py

--- Hero&Mobs.py
+++ Hero&Mobs.py
@@ -1,7 +1,34 @@
 class Hero:
-    """Класс героя"""
-    pass
+
+    def __init__(self, health=100, attack_damage=10, level=1):
+        self.health = health
+        self.attack_damage = attack_damage
+        self.level = level
+        self.inventory = []
+
+    def attack(self, other):
+        pass
+
+    def find_item(self, item):
+        message = f'Вы нашли {item}'
+        self.inventory.append(item)
+        print(message)
 
 
 class Mob:
+
+    def __init__(self, name: str, health=100, attack_damage=10):
+        self.name = name
+        self.attack_damage = attack_damage
+        self.health = health
+
+    def attack(self, other):
+        pass
+
+
+def main():
     pass
+
+
+if __name__ == '__main__':
+    main()
