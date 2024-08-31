df6f8c9847bf75279ffd1ed38d81aebddc32e538
NikolayBar <nikolbar@yandex.ru>
2024-08-31 18:02:09 +0300

    edit Hero&Mobs.py

--- Hero&Mobs.py
+++ Hero&Mobs.py
@@ -1,18 +1,23 @@
 class Hero:
 
     def __init__(self, health=100, attack_damage=10, level=1):
+        self.name = 'Hero'
         self.health = health
         self.attack_damage = attack_damage
         self.level = level
         self.inventory = []
 
     def attack(self, other):
-        pass
+        other.health -= self.attack_damage
+        print(f'{self.name} attacked {other.name}, damage {self.attack_damage}')
 
     def find_item(self, item):
         message = f'Вы нашли {item}'
         self.inventory.append(item)
         print(message)
+
+    def __str__(self):
+        return f'{self.name} {self.health}'
 
 
 class Mob:
@@ -23,11 +28,30 @@ class Mob:
         self.health = health
 
     def attack(self, other):
-        pass
+        other.health -= self.attack_damage
+        print(f'{self.name} нанес {self.attack_damage}')
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.health}'
 
 
 def main():
-    pass
+    def combat(first: Hero, second: Mob):
+        print(f'Status:\n{first.name}: health - {first.health}')
+        print(f'{second.name}: health - {second.health}')
+        first.attack(second)
+        second.attack(first)
+        if first.health <= 0:
+            return f'Победил {second.name}'
+        if second.health <= 0:
+            return f'Победил {first.name}'
+        else:
+            return combat(first, second)
+
+    hero = Hero()
+    mob = Mob('Ork', attack_damage=5)
+    print('Начало схватки')
+    print(combat(hero, mob))
 
 
 if __name__ == '__main__':
